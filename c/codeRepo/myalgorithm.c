c9a476e1f64e594fff5a41f167ff67241d8da719
jeb-ubuntu <www.wangqiuyue2016@gmail.com>
2019-01-11 19:37:55 +0800

    add

--- c/codeRepo/myalgorithm.c
+++ c/codeRepo/myalgorithm.c
@@ -7,6 +7,48 @@
 #include"myalgorithm.h"
 
 
+// scale exchange
+/*将大写字母转换成小写字母*/  
+int tolower(int c)  
+{  
+    if (c >= 'A' && c <= 'Z')  
+    {  
+        return c + 'a' - 'A';  
+    }  
+    else  
+    {  
+        return c;  
+    }  
+} 
+
+//将十六进制的字符串转换成整数  
+int htoi(char s[])  
+{  
+    int i = 0;  
+    int n = 0;  
+    if (s[0] == '0' && (s[1]=='x' || s[1]=='X'))  
+    {  
+        i = 2;  
+    }  
+    else  
+    {  
+        i = 0;  
+    }  
+    for (; (s[i] >= '0' && s[i] <= '9') || (s[i] >= 'a' && s[i] <= 'z') || (s[i] >='A' && s[i] <= 'Z');++i)  
+    {  
+        if (tolower(s[i]) > '9')  
+        {  
+            n = 16 * n + (10 + tolower(s[i]) - 'a');  
+        }  
+        else  
+        {  
+            n = 16 * n + (tolower(s[i]) - '0');  
+        }  
+    }  
+    return n;  
+}  
+
+
 /*将字符串s转换成相应的整数*/  
 int atoi(char s[])  
 {  
@@ -19,6 +61,20 @@ int atoi(char s[])
     return n;  
 }  
 
+//
+void str_reverse(char *s, int start, int end)
+{ 
+    char t; 
+    while(end>start){
+        t=s[start]; 
+        s[start]=s[end]; 
+        s[end]=t;
+        start++; 
+        end--; 
+    }  
+}
+
+// RC4 encryption and decryption
 void rc4_init(uint8_t *s, uint8_t *key, uint16_t Len) //初始化函数
 {
     int i =0, j = 0;
@@ -51,3 +107,29 @@ void rc4_crypt(uint8_t *s, uint8_t *Data, uint16_t Len) //加解密
         Data[k] ^= s[t];
      }
 } 
+
+//Tea 加密函数
+void tea_encrypt (uint32_t* v, uint32_t* k) {
+    uint32_t v0=v[0], v1=v[1], sum=0, i;           /* set up */
+    uint32_t delta=0x9e3779b9;                     /* a key schedule constant */
+    uint32_t k0=k[0], k1=k[1], k2=k[2], k3=k[3];   /* cache key */
+    for (i=0; i < 32; i++) {                       /* basic cycle start */
+        sum += delta;
+        v0 += ((v1<<4) + k0) ^ (v1 + sum) ^ ((v1>>5) + k1);
+        v1 += ((v0<<4) + k2) ^ (v0 + sum) ^ ((v0>>5) + k3);
+    }                                              /* end cycle */
+    v[0]=v0; v[1]=v1;
+}
+
+//Tea 解密函数
+void tea_decrypt (uint32_t* v, uint32_t* k) {
+    uint32_t v0=v[0], v1=v[1], sum=0xC6EF3720, i;  /* set up */
+    uint32_t delta=0x9e3779b9;                     /* a key schedule constant */
+    uint32_t k0=k[0], k1=k[1], k2=k[2], k3=k[3];   /* cache key */
+    for (i=0; i<32; i++) {                         /* basic cycle start */
+        v1 -= ((v0<<4) + k2) ^ (v0 + sum) ^ ((v0>>5) + k3);
+        v0 -= ((v1<<4) + k0) ^ (v1 + sum) ^ ((v1>>5) + k1);
+        sum -= delta;
+    }                                              /* end cycle */
+    v[0]=v0; v[1]=v1;
+}
--- c/codeRepo/myalgorithm.h
+++ c/codeRepo/myalgorithm.h
@@ -4,8 +4,27 @@
 #include<stdbool.h>
 #include<stdlib.h>
 
+// RC4 encryption and decryption
 void rc4_init(uint8_t *s, uint8_t *key, uint16_t Len);//初始化函数
 
 void rc4_crypt(uint8_t *s, uint8_t *Data, uint16_t Len);//加解密
+
+// Tea encryption and decryption
+void tea_encrypt (uint32_t* v, uint32_t* k);//Tea 加密函数
+
+void tea_decrypt (uint32_t* v, uint32_t* k);//Tea 解密函数
+
+// scale exchange
+int tolower(int c);/*将大写字母转换成小写字母*/  
+
+int htoi(char s[]);//将十六进制的字符串转换成整数 
+
+int atoi(char s[]);/*将字符串s转换成相应的整数*/  
+
+
+//
+void str_reverse(char *s, int start, int end);
+
+
 
 #endif
